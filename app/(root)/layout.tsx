850f06c1034dafaea3fdcb5a1600888ce641208d
Booze33 <tebetisloh@gmail.com>
2024-06-21 00:10:45 +0200

    Add left sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,9 +7,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
+        <section>
+          <div>
+            <div>
+              {/* <Image src={} /> */}
+            </div>
+          </div>
+        </section>
         {children}
         <p className="text-whit-1">Right Sidebar</p>
       </main>
--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,10 +1,35 @@
+'use client';
+
+import { SidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 
 const LeftSidebar = () => {
+  const pathname= usePathname();
+  const router = useRouter();
+
   return (
-    <div>
-      
-    </div>
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">Podacstr</h1>
+        </Link>
+
+        {SidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive = pathname === route  || pathname.startsWith(`${route}/`)
+          return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+            'bg-nav-focus border-r-4 border-orange-1': isActive
+          })}>
+            <Image src={imgURL} alt={label} width={24} height={24} />
+            <p>{label}</p>
+          </Link>
+        })}
+      </nav>
+    </section>
   )
 }
 
