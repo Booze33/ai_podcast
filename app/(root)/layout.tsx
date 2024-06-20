bc663c8c7caa57ac8ecbb8838e38221e52ea5977
Booze33 <tebetisloh@gmail.com>
2024-06-20 14:52:12 +0200

    File and folder structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +8,7 @@ export default function RootLayout({
   return (
     <div>
       <main>
-        <p className="text-whit-1">Left Sidebar</p>
+        <LeftSidebar />
         {children}
         <p className="text-whit-1">Right Sidebar</p>
       </main>
