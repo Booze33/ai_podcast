db36b1c49b736d6446fc195f28a066cdd811dac7
Booze33 <tebetisloh@gmail.com>
2024-06-22 14:30:16 +0200

    Home layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -10,15 +12,26 @@ export default function RootLayout({
     <div className="relative flex flex-col">
       <main className="relative flex bg-black-3">
         <LeftSidebar />
-        <section>
-          <div>
-            <div>
-              {/* <Image src={} /> */}
+
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md;hidden">
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              <MobileNav />
+            </div>
+            <div className="flex flex-col md:pb-14">
+              Toaster (notification popups)
+              {children}
             </div>
           </div>
         </section>
-        {children}
-        <p className="text-whit-1">Right Sidebar</p>
+        
+        <RightSidebar />
       </main>
     </div>
   );
