9775a16233ad8b25ab78fcea0cee82d3f64d6a1a
Booze33 <tebetisloh@gmail.com>
2024-06-24 16:03:51 +0200

    Create Podcats Page

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const GeneratePodcast = ({
+  setAudioStorageId,
+  setAudio,
+  voiceType,
+  audio,
+  voicePrompt,
+  setVoicePrompt,
+  setAudioDuration
+  ,}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GeneratePodcast
