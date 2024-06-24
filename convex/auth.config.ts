a65da590a90aed4decdcbb907a8b4bf2d5124fd9
Booze33 <tebetisloh@gmail.com>
2024-06-24 02:09:38 +0200

    Clerk setup and authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://sweet-reindeer-4.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
