b8b3f25e18ef42d93a209b559c1b20ea96ff239e
Booze33 <tebetisloh@gmail.com>
2024-06-26 06:43:35 +0200

    integrate openai for thumbnail

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'fortunate-minnow-399.convex.cloud'
       }
     ]
   }
