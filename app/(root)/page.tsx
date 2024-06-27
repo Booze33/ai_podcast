c78a142b60574441dd2c02b0b653b5ef2a96a69d
Booze33 <tebetisloh@gmail.com>
2024-06-27 03:20:16 +0200

    Podcast Details Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,27 @@
 "use client";
-
 import PodcastCard from '@/components/PodcastCard'
-import { Button } from '@/components/ui/button'
-import { podcastData } from '@/constants'
-import React from 'react'
 import { useQuery } from "convex/react";
-import { api } from '@/convex/_generated/api';
+import { api } from "@/convex/_generated/api";
+import LoaderSpinner from '@/components/LoaderSpinner';
 
 const Home = () => {
-  // const tasks = useQuery(api.tasks.get);
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
+  if(!trendingPodcasts) return <LoaderSpinner />
+  
   return (
-    <div className="mt-9 flex flex-col gap-9">
-      <section className="flex flex-col gap-5">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
+      <section className='flex flex-col gap-5'>
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
-        </div>
+
         <div className="podcast_grid">
-          {podcastData.map(({ id, title, description, imgURL }) => (
-            <PodcastCard
-              key={id}
-              imgUrl={imgURL}
-              title={title}
-              description={description}
-              podcastId={id}
+          {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+            <PodcastCard 
+              key={_id}
+              imgUrl={imageUrl as string}
+              title={podcastTitle}
+              description={podcastDescription}
+              podcastId={_id}
             />
           ))}
         </div>
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'fortunate-minnow-399.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
       }
     ]
   }
