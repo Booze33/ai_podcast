24e98b1f8b268eaf3475d6184f00bcfa545de08d
Booze33 <tebetisloh@gmail.com>
2024-06-28 03:11:19 +0200

    Podcast right side bar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -6,10 +6,15 @@ import Header from './Header';
 import Carousel from './Carousel';
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import LoaderSpinner from './LoaderSpinner';
 
 const RightSidebar = () => {
   const { user } = useUser();
-  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount)
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+  const router = useRouter();
+
+  if(!topPodcasters) return <LoaderSpinner />
 
   return (
     <section className="right_sidebar text-white-1">
@@ -31,6 +36,28 @@ const RightSidebar = () => {
       <section>
         <Header headerTitle="Fans Like You" />
         <Carousel fansLikeDetail={topPodcasters!} />
+      </section>
+      <section className="flex flex-col gap-8 pt-12">
+        <Header headerTitle="Top Podcastrs" />
+        <div className="flex flex-col gap-6">
+          {topPodcasters?.slice(0, 4).map((podcaster) => (
+            <div key={podcaster._id} className="flex cursor-pointer justify-between" onClick={() => router.push(`/profile/${podcaster.clerkId}`)}>
+              <figure className="flex items-center gap-2">
+                <Image
+                  src={podcaster.imageUrl}
+                  alt={podcaster.name}
+                  width={44}
+                  height={44}
+                  className="aspect-square rounded-lg"
+                />
+                <h2 className="text-14 font-semibold text-white-1">{podcaster.name}</h2>
+              </figure>
+              <div className="flex items-center">
+                <p className="text-12 font-normal">{podcaster.totalPodcasts} podcats</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </section>
   )
